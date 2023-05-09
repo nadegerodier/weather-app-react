c9eacda9cb7621d69888677c415f5212601ed2be
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2023-05-09 18:17:19 -0500

    Small changes

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useRef } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
